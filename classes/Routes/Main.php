dc85e4508867a7e7968c68ad7e9d8df6fd449f18
Gnieark <gnieark@free.fr>
2021-11-07 20:09:07 +0000

    HEY!

--- classes/Routes/Main.php
+++ classes/Routes/Main.php
@@ -2,6 +2,11 @@
 Class Main extends Route
 {
     protected static $displayOnPage = true;
+    static public function get_content_html(PDO $db, User $user)
+    {
+        $tpl = new TplBlock();
 
+        return $tpl->applyTplFile("../templates/main.html");
+    }
 
 }
