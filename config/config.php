91291c6e18c7388133b7e88fd6609b00b54d72bd
Gnieark <gineark@free.fr>
2021-11-07 19:23:20 +0100

    jkhgh

--- config/config.php
+++ config/config.php
@@ -1,1 +1,8 @@
 <?php
+return (object) array(
+        'sql_params_local_db' => array( 
+        "dsn"           => "mysql:dbname=domognieark;host=localhost",
+        "user"          => "domognieark",
+        "password"      => "changeme",
+    )
+);
