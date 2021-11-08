0fce47dcef351ddac26a219c6bb66d3728ae7af4
gnieark <gnieark@free.fr>
2021-11-08 13:42:51 +0100

    js

--- classes/Routes/DevicesManager.php
+++ classes/Routes/DevicesManager.php
@@ -2,7 +2,18 @@
 
 class DevicesManager extends Route{
 
+    static public function get_content_html(PDO $db, User $user)
+    {
+        $tpl = new TplBlock();
 
 
+
+        return $tpl->applyTplFile("../templates/DevicesManager.html");
+    }
+    
+    static public function get_custom_js()
+    {
+        return file_get_contents("../templates/DevicesManager.js");
+    }
 
 }
