ec2406d8fcfa99ad50b92f840e0b27104b0cee57
Gnieark <gnieark@free.fr>
2021-11-17 22:58:58 +0000

    à ces

--- templates/dashboard.js
+++ templates/dashboard.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     //the event occurred
-    document.querySelector(".switchButton").getSwitchStatus();
+    document.querySelector(".switchButton").updateSwitchStatus();
     document.querySelector(".switchButton").addEventListener('click', changeStatus);
 })
 
-function getSwitchStatus(){
-    
+function updateSwitchStatus(){
+
 }
